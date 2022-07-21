cdd2fdecb9191b948046a2f447be1943f4e98bc9
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2022-07-21 14:16:48 -0800

    Setup libqodot in project tree.

--- package.py
+++ package.py
@@ -19,3 +19,9 @@ os.chdir("EngineSrc-godot/bin")
 subprocess.run(["godot.windows.opt.tools.64.mono.exe", "--path",  "../../Game", "--export", "Windows-CustomFork", "../bin/{}/CyberStealth.exe".format(target)])
 shutil.copyfile("mono-2.0-sgen.dll", "../../bin/{}/mono-2.0-sgen.dll".format(target))
 shutil.copytree("data.mono.windows.64.release/", "../../bin/{}/data.mono.windows.64.release/".format(target), dirs_exist_ok=True)
+
+# Copy over libqodot files
+os.chdir("../../qodot-plugin/libqodot")
+subprocess.run(["scons", "p=windows", "target={}".format(target)])
+shutil.copyfile("libmap/build/libmap.dll", "../../bin/{}/libmap.dll".format(target))
+shutil.copyfile("build/libqodot.dll", "../../bin/{}/libqodot.dll".format(target))
