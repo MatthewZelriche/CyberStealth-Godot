9ef0534f04dbdbf7b87b656067cd975acba15484
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2022-08-05 16:50:08 -0800

    More finishing touches to 3.5 update.

--- package.py
+++ package.py
@@ -22,12 +22,6 @@ subprocess.run(["godot.windows.opt.tools.64.mono.exe", "--path",  "../../Game", 
 shutil.copyfile("mono-2.0-sgen.dll", "../../bin/{}/mono-2.0-sgen.dll".format(target))
 shutil.copytree("data.mono.windows.64.release/", "../../bin/{}/data.mono.windows.64.release/".format(target), dirs_exist_ok=True)
 
-# Copy over libqodot files
-os.chdir("../../qodot-plugin/libqodot")
-subprocess.run(["scons", "p=windows", "target=release".format(target)])
-shutil.copyfile("libmap/build/libmap.dll", "../../bin/{}/libmap.dll".format(target))
-shutil.copyfile("build/libqodot.dll", "../../bin/{}/libqodot.dll".format(target))
-
 # Copy over trenchbroom files
 os.chdir("../../Game/")
 shutil.copytree("trenchbroom_data/", "../bin/{}/trenchbroom_data/".format(target), dirs_exist_ok=True)
